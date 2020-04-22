9cbca5087bd93d0d072c28bda6d2dba3bb322901
Yurii Popovych <popovychyb@gmail.com>
2020-04-22 22:29:56 +0300

    added base realisation

---
+++ src/main/java/mate/academy/internetshop/dao/ItemDao.java
@@ -0,0 +1,15 @@
+package mate.academy.internetshop.dao;
+
+import mate.academy.internetshop.model.Item;
+
+public interface ItemDao {
+    Item create(Item item);
+
+    Item get(Long id);
+
+    Item update(Item item);
+
+    void delete(Long id);
+
+    void delete(Item item);
+}
---
+++ src/main/java/mate/academy/internetshop/service/impl/BucketServiceImpl.java
@@ -0,0 +1,21 @@
+package mate.academy.internetshop.service.impl;
+
+import mate.academy.internetshop.dao.BucketDao;
+import mate.academy.internetshop.dao.ItemDao;
+import mate.academy.internetshop.model.Bucket;
+import mate.academy.internetshop.model.Item;
+import mate.academy.internetshop.service.BucketService;
+
+public class BucketServiceImpl implements BucketService {
+
+    private BucketDao bucketDao;
+    private ItemDao itemDao;
+
+    @Override
+    public Bucket addItem(Long bucketId, Long itemId) {
+        Bucket bucket = bucketDao.get(bucketId);
+        Item item = itemDao.get(itemId);
+        bucket.getItems().add(item);
+        return bucketDao.update(bucket);
+    }
+}
