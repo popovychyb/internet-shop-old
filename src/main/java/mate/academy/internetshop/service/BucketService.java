0ce314d9f86af744c56f8d71a5fea46c4f90041d
Yurii Popovych <popovychyb@gmail.com>
2020-04-23 10:18:30 +0300

    deleted redundant classes

--- src/main/java/mate/academy/internetshop/service/BucketService.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.internetshop.service;
-
-import mate.academy.internetshop.model.Bucket;
-
-public interface BucketService {
-    Bucket addItem(Long bucketId, Long productId);
-}
