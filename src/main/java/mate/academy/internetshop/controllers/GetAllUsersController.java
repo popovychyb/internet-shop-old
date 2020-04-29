a938b206aa2493150d8a115d822692e98516e07b
Yurii Popovych <popovychyb@gmail.com>
2020-04-29 16:10:26 +0300

    renamed userController

--- src/main/java/mate/academy/internetshop/controllers/GetAllUsersController.java
+++ src/main/java/mate/academy/internetshop/controllers/GetAllUsersController.java
@@ -10,7 +10,7 @@ import mate.academy.internetshop.lib.Injector;
 import mate.academy.internetshop.model.User;
 import mate.academy.internetshop.service.UserService;
 
-public class UsersController extends HttpServlet {
+public class GetAllUsersController extends HttpServlet {
     private static final Injector INJECTOR = Injector.getInstance("mate.academy.internetshop");
     private UserService userService = (UserService) INJECTOR.getInstance(UserService.class);
 
